716223fd301aea9c31eb85fb026ea6de17c6ba76
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-17 20:33:37 +1000

    added ingredients to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+# Create users
+
 10.times do
     User.create(username: Faker::Name.name )
 end
+
+# Create recipes
 
 10.times do |i|
     3.times do |j|
@@ -9,10 +13,18 @@ end
     end
 end
 
+# Create favourites
+
 User.all.each do |user| 
     Favourite.create(user_id: user.id)
 
     5.times do 
         user.favourite.favourites_recipes.create(recipe_id: rand(1..Recipe.all.length))
     end
+end
+
+# Create ingredients
+
+10.times do 
+    Ingredient.create(name:Faker::Food.ingredient)
 end
