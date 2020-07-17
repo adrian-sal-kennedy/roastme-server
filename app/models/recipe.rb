f39d69178b80ae8750ca5c0e2ee174a56d38f2dd
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-17 21:29:45 +1000

    fixed typo in association for recipe's tags

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,7 @@ class Recipe < ApplicationRecord
     has_many :favourites, through: :favourites_recipes
 
     has_many :recipes_tags
-    has_many :tag, through: :recipes_tags
+    has_many :tags, through: :recipes_tags
 
     has_many :recipes_ingredients
     has_many :ingredients, through: :recipes_ingredients
