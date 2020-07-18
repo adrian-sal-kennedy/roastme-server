9afcbd25ab810a8e9cb45e67a92cc18c0ff0c944
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-18 23:46:07 +1000

    added query string to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render json: {controller:"home", action:"index"}
-
+    render json: {controller:"home", action:"index", query_string:request.query_parameters}
   end
 end
