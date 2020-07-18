9c48efc2012f5acbca82c5df468d3c0cad28f0d2
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-18 23:19:54 +1000

    added all needed routes for MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  # test route/controller
+
   get '/test', to: "test#test"
+
+  # home
+
+  get '/' 
+
+  # cookbook
+  
+  get '/cookbook'
+
+  # dashboard
+
+  get '/dashboard'
+
+  # profile
+
+  get '/profile'
+
+  # users
+
+  get '/user/:id'
+
+  # authentication
+
+  post '/login'
+  post '/signup'
+
+  # posts
+
+  get '/post/:id'
+  post '/post'
+  patch '/post/:id'
+  delete '/post/:id'
+
+  # recipe
+
+  get '/post/:id'
+  post '/post'
+  patch '/post/:id'
+  delete '/post/:id'
+
 end
