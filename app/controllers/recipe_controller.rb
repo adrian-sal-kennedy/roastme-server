67ac7dcf7e992e42bcfb176ef295b948a45308a0
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-20 23:03:48 +1000

    working towards recipe CRUD

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -2,22 +2,30 @@ class RecipeController < ApplicationController
   before_action :authenticate_user, except: :show
 
   def show
-    render json: {controller:"recipe", action:"show"}
+
+    render json: Recipe.find(params[:id])
 
   end
 
   def delete
-    render json: {controller:"recipe", action:"delete"}
-
+    recipe = Recipe.find(params[:id])
+    if recipe.user == current_user
+      recipe.destroy
+      render json: {post: params[:id], deleted: true}
+    else
+      render json: {post: params[:id], deleted: false}
+    end
   end
 
   def update
-    render json: {controller:"recipe", action:"update"}
-
+    if Recipe.find(params[:id]).user == current_user
+      render json: {controller:"recipe", action:"update"}
+    else
+      render json: {error: true}
+    end
   end
 
   def create
     render json: {controller:"recipe", action:"create"}
-
   end
 end
