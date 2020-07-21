c1f8d1403ea290775d9937c0036c9ca80072abbb
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-21 18:06:35 +1000

    please work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # query = request.query_parameters
-    # page = query['page'] || 1
-
-    render json: Recipe.limit(10)
+    render json: Recipe.all
   end
 end
