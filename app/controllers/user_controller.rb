2807fac5c9f0aa6555f81626d6b73d1732435930
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-07-22 15:50:16 +1000

    check if user email exists

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,13 @@
 class UserController < ApplicationController
-
+  before_action :check_existing, only: :create
   def create
-    User.create(user_params)
-    render json: "user created", status: 200
+  # would use find_or_create_by here, but it is not atomic and doesn't return good stuff so it's more appropriate in seeds.rb or something
+    unless @record
+      User.create(user_params)
+      render json: "user created", status: 200
+    else
+      render json: "user already exists!", status: 401
+    end
   end
 
   def show
@@ -10,7 +15,11 @@ class UserController < ApplicationController
     
   end
   private 
-  
+  def check_existing
+    @record = User.find_by(email: params[:user][:email])
+    puts "found record #{params[:user][:email]}" if @record
+    # storing record in variable so rails caches the query
+  end
   def user_params 
     params.require(:user).permit(:email, :password, :username)
   end 
