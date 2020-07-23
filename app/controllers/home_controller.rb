712b3a60fedc86edb81215054e99acf5a20dad41
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-23 11:08:36 +1000

    added a limit to home controller (temp)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     recipes = []
-    Recipe.all.each do |recipe|
+    Recipe.limit(10).each do |recipe|
       recipes << {recipe: recipe, author: recipe.user, tags: recipe.tags}
     end
     render json: recipes
