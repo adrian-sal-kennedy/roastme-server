4d81cce9ca2e0614c940bee64142b11b92236543
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-27 12:05:30 +1000

    removed weird logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,11 +2,7 @@ class UserController < ApplicationController
 
   def create
     User.create(user_params)
-<<<<<<< HEAD
-    render json: "user created"
-=======
     render json: "user created", status: 200
->>>>>>> parent of b484e79... Merge branch 'development' of github.com:Guy-Norkunas/roastme-server into guy
   end
 
   def show
@@ -15,10 +11,7 @@ class UserController < ApplicationController
     
   end
   private 
-<<<<<<< HEAD
-=======
   
->>>>>>> parent of b484e79... Merge branch 'development' of github.com:Guy-Norkunas/roastme-server into guy
   def user_params 
     params.require(:user).permit(:email, :password, :username)
   end 
