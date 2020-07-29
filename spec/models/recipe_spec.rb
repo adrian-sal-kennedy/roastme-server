c6e3b785caa563a474b8a1c9a71c12aab417bb73
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-29 22:40:53 +1000

    added validation and association tests for recipe_spec

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,5 +5,40 @@ RSpec.describe Recipe, type: :model do
     build :recipe
   }
 
+  context "validations" do
+    it "should validate presence of title" do
+      expect(subject).to be_valid
+    end
 
+    it "should validate presence of title" do
+      expect(subject).to validate_presence_of(:title)
+    end
+
+    it "should validate presence of title" do
+      expect(subject).to validate_presence_of(:blog)
+    end
+
+    it "should validate presence of title" do
+      expect(subject).to validate_presence_of(:method)
+    end
+  end
+
+  context "associations" do
+
+    it "should have many recipes_ingredients" do
+      expect(subject).to have_many(:recipes_ingredients)
+    end
+
+    it "should have many ingredients" do
+      expect(subject).to have_many(:ingredients)
+    end
+
+    it "should have many recipes_tags" do
+      expect(subject).to have_many(:recipes_tags)
+    end
+
+    it "should have many tags" do
+      expect(subject).to have_many(:tags)
+    end
+  end
 end
