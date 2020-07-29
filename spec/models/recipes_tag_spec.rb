16c663032fe809910cf0ded9b8bf47950e1df9f5
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 00:32:47 +1000

    added tests for recipe-tag table

--- spec/models/recipes_tag_spec.rb
+++ spec/models/recipes_tag_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RecipesTag, type: :model do
-  subject {
-    build :recipes_tag
-  }
-
   context "associations" do
     it "should belong to a recipe" do
-      expect(subject).to belong_to(:recipe)
+      should belong_to(:recipe)
     end
 
     it "should belong to a tag" do
-      expect(subject).to belong_to(:tag)
+      should belong_to(:tag)
     end
   end
 end
