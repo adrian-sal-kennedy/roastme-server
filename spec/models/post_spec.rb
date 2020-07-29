617baa743de447ff6d00b54cea7144f3a7c8d76a
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-29 22:40:17 +1000

    added association tests for post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  subject {
+    build :tag
+  }
+
+  context "validations" do
+    it "should validate presence of tag" do
+      expect(subject).to validate_presence_of(:tag)
+    end
+  end
+
+  context "associations" do
+    it "should have many recipe_tags" do
+      expect(subject).to have_many(:recipes_tags)
+    end
+
+    it "should have many recipes" do
+      expect(subject).to have_many(:recipes)
+    end
+  end
 end
