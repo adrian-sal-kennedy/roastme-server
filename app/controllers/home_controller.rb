6fd1030be01d0f70d989df9b9a7319c01f42022c
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-29 16:58:57 +1000

    fixed output to match previous format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,15 +8,32 @@ class HomeController < ApplicationController
     # qparams["offset"] eg. 15 (starts at 16th entry, after sorting)
     # qparams["order"] eg. old/new, favourites, alphabetical (sorts based on string)
 
+    # recipes = []
+    # Recipe.all.each do |recipe|
+    #   recipes << {recipe: recipe, author: recipe.user, tags: recipe.tags}
+    # end
+    # render json: recipes
 
     if qparams["tag"] && qparams["ingredient"]
-      render json: Recipe.joins(:tags).where("tags.tag LIKE ?", qparams["tag"]).joins(:ingredients).where("ingredients.name LIKE ?", qparams["ingredient"]).limit(qparams["limit"] || 20).offset(qparams["offset"] || 0)
+      render json: list(Recipe.joins(:tags).where("tags.tag LIKE ?", qparams["tag"]).joins(:ingredients).where("ingredients.name LIKE ?", qparams["ingredient"]).limit(qparams["limit"] || 20).offset(qparams["offset"] || 0))
     elsif qparams["tag"]
-      render json: Recipe.joins(:tags).where("tags.tag LIKE ?", qparams["tag"]).limit(qparams["limit"]).offset(qparams["offset"])
+      render json: list(Recipe.joins(:tags).where("tags.tag LIKE ?", qparams["tag"]).limit(qparams["limit"]).offset(qparams["offset"]))
     elsif qparams["ingredient"]
-      render json: Recipe.joins(:ingredients).where("ingredients.name LIKE ?", qparams["ingredient"]).limit(qparams["limit"]).offset(qparams["offset"])
+      render json: list(Recipe.joins(:ingredients).where("ingredients.name LIKE ?", qparams["ingredient"]).limit(qparams["limit"]).offset(qparams["offset"]))
     else
-      render json: Recipe.limit(qparams["limit"]).offset(qparams["offset"])
+      render json: list(Recipe.limit(qparams["limit"]).offset(qparams["offset"]))
     end
   end
+
+  private
+
+  def list(recipes)
+    list = []
+    recipes.each do |recipe|
+      list << {recipe: recipe, author: recipe.user, tags: recipe.tags}
+    end
+
+    return list
+  end
+
 end
