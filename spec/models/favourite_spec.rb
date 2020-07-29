799e5349bddb1cbb1e4e420402d23821940fe99f
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 00:28:23 +1000

    added all tests for favourite

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Favourite, type: :model do
+  context "associations" do
+    it "should belong to a user" do
+      should belong_to(:user)
+    end
+
+    it "should have many favourite_recipes" do
+      should have_many(:favourites_recipes)
+    end
+
+    it "should have many recipes" do
+      should have_many(:recipes)
+    end
+  end
 end
