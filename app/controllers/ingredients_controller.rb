215d671d4a3e6ca4f1ba8be34cbb1e0d8df0d56e
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-07-29 23:45:36 +1000

    add recipe id to ingredient create output

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -34,7 +34,7 @@ class IngredientsController < ApplicationController
     end
 
     if count > 0
-      render json: { total: count, ingredients: added_ingredients }
+      render json: { total: count, ingredients: added_ingredients, recipe_id: recipe }
     else
       render json: { total: count, unsuccessful: true }
     end
