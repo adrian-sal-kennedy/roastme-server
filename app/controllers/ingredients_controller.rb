f23c1ba853e3d9659b26630062437fa663a359f4
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-29 14:50:22 +1000

    now creates new ingredient whenever it doesn't recognise ingedient in database

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,9 +7,35 @@ class IngredientsController < ApplicationController
   end
 
   def create
-    recipe = Recipe.find(params[:id]).user.id
-    if current_user.id == recipe
-      pp params.require(:ingredients).permit(:list)
+    ingredients = Ingredient.order(name: :asc).pluck(:name)
+    recipe = Recipe.find(params[:id])
+    count = 0
+    added_ingredients = []
+
+
+    if current_user.id == recipe.user.id
+      params.require(:ingredients).permit(:list)[:list].split(",").each do |name|
+        ingredient = Ingredient.find_by_name(name)
+
+        if ingredient
+          recipe.recipes_ingredients.create(ingredient_id: ingredient.id)
+          count += 1
+
+        else
+          ingredient = Ingredient.create(name: name)
+          recipe.recipes_ingredients.create(ingredient_id: ingredient.id)
+          count += 1
+        end
+
+        added_ingredients << ingredient
+
+      end
+    end
+
+    if count > 0
+      render json: {total: count, ingredients: added_ingredients}
+    else
+      render json: {total: count, unsuccessful: true}
     end
   end
 
