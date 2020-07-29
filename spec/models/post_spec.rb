73e446227f665d4cfa0c59d0bb4186d43fff44fe
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 00:31:36 +1000

    added tests for posts table

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Post, type: :model do
 
   context "validations" do
     it "should be valid with valid attributes" do
-
+      expect(subject).to be_valid
     end
 
     it "should validate presence of post" do
-      expect(subject).to validate_presence_of(:post)
+      should validate_presence_of(:post)
     end
   end
 
   context "associations" do
-    it "should have many recipe_tags" do
-      expect(subject).to have_many(:recipes_tags)
+    it "should belong to user" do
+      should belong_to(:user)
     end
 
-    it "should have many recipes" do
-      expect(subject).to have_many(:recipes)
+    it "should belong to recipe" do
+      should belong_to(:recipe)
     end
   end
 end
