4579ab53605fe896970a2ed6484fe28384a22b27
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-30 00:32:16 +1000

    added tests for recipe ingredient table

--- spec/models/recipes_ingredient_spec.rb
+++ spec/models/recipes_ingredient_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RecipesIngredient, type: :model do
-  subject {
-    build :recipes_ingredient
-  }
-
   context "associations" do
     it "should belong to a recipe" do
-      expect(subject).to belong_to(:recipe)
+      should belong_to(:recipe)
     end
 
     it "should belong to a ingredient" do
-      expect(subject).to belong_to(:ingredient)
+      should belong_to(:ingredient)
     end
   end
 end
