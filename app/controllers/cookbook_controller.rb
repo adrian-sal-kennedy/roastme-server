3a444122aeaed6b10b1c1e9d26cf629bf5e2b786
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-08-03 14:50:49 +1000

    have cookbook sorting

--- app/controllers/cookbook_controller.rb
+++ app/controllers/cookbook_controller.rb
@@ -4,12 +4,36 @@ class CookbookController < ApplicationController
   before_action :authenticate_user
 
   def index
-    temp = []
+    qparams = request.query_parameters
+    list = []
     recipes = current_user.recipes + current_user.favourite.recipes
-    recipes.each do |recipe|
-      temp << { recipe: recipe, author: recipe.user, tags: recipe.tags }
+
+    recipes = recipes.joins(:tags).where('tags.tag LIKE ?', qparams['tag']) if qparams['tag']
+    if qparams['ingredient']
+      recipes = recipes.joins(:ingredients).where('ingredients.name LIKE ?', qparams['ingredient'])
     end
-    pp temp
-    render json: { list: temp, count: temp.length }
+    recipes = recipes.where('title LIKE ?', "%#{qparams['search']}%") if qparams['search']
+
+    if qparams['order']
+      case qparams['order']
+      when 'old'
+        recipes = recipes.order('created_at DESC')
+      when 'new'
+        recipes = recipes.order('created_at ASC')
+      when 'alpha'
+        recipes = recipes.order('title ASC')
+      when 'alpha-rev'
+        recipes = recipes.order('title DESC')
+      end
+    end
+
+    recipes = recipes.limit(qparams['limit']) if qparams['limit']
+    recipes = recipes.offset(qparams['offset']) if qparams['offset']
+
+
+    recipes.each do |recipe|
+      list << { recipe: recipe, author: recipe.user, tags: recipe.tags }
+    end
+    render json: { list: list, count: list.length }
   end
 end
